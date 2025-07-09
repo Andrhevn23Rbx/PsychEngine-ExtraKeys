ec52aa37649b547810544932f9e46252513588e4
Andre Nicholas Jr <151701402+Andrhevn23Rbx@users.noreply.github.com>
2025-07-09 16:30:04 -0500

    Update Note.hx
    e

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -81,41 +81,72 @@ class Note extends FlxSprite
 	public var lateHitMult:Float = 1;
 	public var lowPriority:Bool = false;
 
-	public static var SUSTAIN_SIZE:Int = 44;
-	public static var swagWidth:Float = 160 * 0.7;
-	public static var colArray:Array<String> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-	public static var colArrayAlt:Array<String> = ['purple', 'blue', 'green', 'red', 'white', 'yellow', 'violet', 'black', 'dark'];
-	public static var pressArrayAlt:Array<String> = ['left', 'down', 'up', 'right', 'white', 'yellow', 'violet', 'black', 'dark'];
-	public static var defaultNoteSkin(default, never):String = 'noteSkins/NOTE_assets';
+public static var SUSTAIN_SIZE:Int = 44;
+public static var swagWidth:Float = 160 * 0.7;
 
-	public static var scales:Array<Float> = [0.7, 0.7, 0.7, 0.7, 0.65, 0.6, 0.55, 0.5, 0.46];
-	public static var scalesPixel:Array<Float> = [1, 1, 1, 1, 0.93, 0.86, 0.79, 0.71, 0.66];
-	public static var splashOffsetScale:Array<Float> = [1, 1, 1, 1, 1.08, 1.17, 1.27, 1.4, 1.52];
-	public static var swidths:Array<Float> = [112, 112, 112, 112, 98, 84, 77, 70, 63];
-	public static var posRest:Array<Int> = [-168, -112, -56, 0, 15, 35, 45, 55, 60];
-	public static var midArray:Array<Int> = [0, 0, 1, 1, 2, 2, 3, 3, 4];
-	public static var gfxIndex:Array<Dynamic> = [
-		[4],
-		[0, 3],
-		[0, 4, 3],
-		[0, 1, 2, 3],
-		[0, 1, 4, 2, 3],
-		[0, 2, 3, 5, 1, 8],
-		[0, 2, 3, 4, 5, 1, 8],
-		[0, 1, 2, 3, 5, 6, 7, 8],
-		[0, 1, 2, 3, 4, 5, 6, 7, 8]
-	];
-	public static var gfxHud:Array<Dynamic> = [
-		[4],
-		[0, 3],
-		[0, 4, 3],
-		[0, 1, 2, 3],
-		[0, 1, 4, 2, 3],
-		[0, 2, 3, 0, 1, 3],
-		[0, 2, 3, 4, 0, 1, 3],
-		[0, 1, 2, 3, 0, 1, 2, 3],
-		[0, 1, 2, 3, 4, 0, 1, 2, 3]
-	];
+public static var colArray:Array<String> = [
+	'A','B','C','D','E','F','G','H','I','J','K','L','M',
+	'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+public static var colArrayAlt:Array<String> = [
+	'notea','noteb','notec','noted','notee','notef','noteg','noteh','notei',
+	'notej','notek','notel','notem','noten','noteo','notep','noteq','noter',
+	'notes','notet','noteu','notev','notew','notex','notey','notez'
+];
+public static var pressArrayAlt:Array<String> = [
+	'left','down','up','right','white','yellow','violet','black','dark',
+	'keyj','keyk','keyl','keym','keyn','keyo','keyp','keyq','keyr',
+	'keys','keyt','keyu','keyv','keyw','keyx','keyy','keyz'
+];
+public static var defaultNoteSkin(default, never):String = 'noteSkins/NOTE_assets';
+
+public static var scales:Array<Float> = [
+	0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
+	0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7,
+	0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7
+];
+public static var scalesPixel:Array<Float> = [
+	1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1
+];
+public static var splashOffsetScale:Array<Float> = [
+	1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1
+];
+public static var swidths:Array<Float> = [
+	64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64
+];
+public static var posRest:Array<Int> = [
+	-168, -160, -152, -144, -136, -128, -120, -112, -104,
+	-96, -88, -80, -72, -64, -56, -48, -40, -32,
+	-24, -16, -8, 0, 8, 16, 24, 32
+];
+public static var midArray:Array<Int> = [
+	0, 0, 1, 1, 2, 2, 3, 3, 4,
+	4, 5, 5, 6, 6, 7, 7, 8, 8,
+	9, 9, 10, 10, 11, 11, 12, 12
+];
+
+// Updated for 26 keys (defaulted with cycling visual index references)
+public static var gfxIndex:Array<Dynamic> = [
+	[
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7, 8,
+		0, 1, 2, 3, 4, 5, 6, 7
+	]
+];
+public static var gfxHud:Array<Dynamic> = [
+	[
+		0, 1, 2, 3, 0, 1, 2, 3, 0,
+		1, 2, 3, 0, 1, 2, 3, 0, 1,
+		2, 3, 0, 1, 2, 3, 0, 1
+	]
+];
+
 	public static var gfxDir:Array<String> = ['LEFT', 'DOWN', 'UP', 'RIGHT', 'SPACE'];
 	public static var dataNum:Int;
 
